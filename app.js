90c21bc720d2325f8ad099772bbb01fbbb5c9a82
Vedanta Saha <2024etb021.vedanta@students.iiests.ac.in>
2025-08-26 09:26:47 +0530

    fixed ham menu

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-const video1 = document.getElementById('projectVideo1');
-const video2 = document.getElementById('projectVideo2');
-const video3 = document.getElementById('projectVideo3');
+
 
 // Sidebar elements //
 const sideBar = document.querySelector('.sidebar');
@@ -10,18 +8,18 @@ const closeIcon = document.querySelector('.close-icon')
 
 const hoverSign = document.querySelector('.hover-sign');
 
-const videoList =[video1, video2, video3];
+// const videoList =[video1, video2, video3];
 
-videoList.forEach (function(video){
-    video.addEventListener("mouseover", function(){
-        video.play()
-        hoverSign.classList.add("active")
-    })
-    video.addEventListener("mouseout", function(){
-    video.pause();
-    hoverSign.classList.remove("active")
-})
-})
+// videoList.forEach (function(video){
+//     video.addEventListener("mouseover", function(){
+//         video.play()
+//         hoverSign.classList.add("active")
+//     })
+//     video.addEventListener("mouseout", function(){
+//     video.pause();
+//     hoverSign.classList.remove("active")
+// })
+// })
 
 // Sidebar elements //
 menu.addEventListener("click", function(){
